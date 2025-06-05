257e969a5bdd0206d55b487ca7bce039aba84009
Daniel <danielneryq@gmail.com>
2025-06-05 12:02:13 -0300

    Update script-02.js

--- script-02.js
+++ script-02.js
@@ -3,5 +3,35 @@ function soma(){
   var b = parseFloat(document.getElementById('valor02').value);
   //document.getElementById('demo').innerHTML = a + b;
   document.getElementById('demo').innerHTML = a + b;
-  
+
+}
+
+function subtrai(){
+  var a = parseFloat(document.getElementById('valor01').value);
+  var b = parseFloat(document.getElementById('valor02').value);
+  document.getElementById('demo02').innerHTML = a - b;
+
+}
+
+function multiplica(){
+  var a = parseFloat(document.getElementById('valor01').value);
+  var b = parseFloat(document.getElementById('valor02').value);
+  document.getElementById('demo03').innerHTML = a * b;
+
+}
+
+function divide(){
+  var a = parseFloat(document.getElementById('valor01').value);
+  var b = parseFloat(document.getElementById('valor02').value);
+  if (b === 0) {
+    document.getElementById('demo04').innerHTML = "divisão por zero!";
+  } else {
+    document.getElementById('demo04').innerHTML = a/b;
+  }
+}
+
+function potencia(){
+  var a = parseFloat(document.getElementById('valor01').value);
+  var b = parseFloat(document.getElementById('valor02').value);
+  document.getElementById('demo05').innerHTML = math.pow(a, b);
 }
